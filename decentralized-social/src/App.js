5c5e140ee54b2ad83d9d2fc9a4a3a79a19c7adb0
amatureApe <amatureApe@gmail.com>
2022-10-14 20:44:28 -0500

    add lens api calls

--- decentralized-social/src/App.js
+++ decentralized-social/src/App.js
@@ -1,6 +1,57 @@
-import './App.css';
+import { useEffect, useState } from 'react';
+import {
+  urlClient,
+  LENS_HUB_CONTRACT_ADDRESS,
+  queryRecommendedProfiles,
+  queryExplorePublications
+} from './queries';
+import LENSHUB from './lenshub';
+import { ethers } from 'ethers';
+import { Box, Button, Image } from "@chakra-ui/react"
 
 function App() {
+  const [account, setAccount] = useState(null);
+  const [profiles, setProfiles] = useState([]);
+  const [posts, setPosts] = useState([]);
+
+  async function signIn() {
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts"
+    });
+    setAccount(accounts[0]);
+  }
+
+  async function getRecommendedProfiles() {
+    const response = await urlClient.query(queryRecommendedProfiles).toPromise();
+    const profiles = response.data.recommendedProfiles.slice(0, 5);
+    setProfiles(profiles);
+  }
+
+  async function getPosts() {
+    const response = await urlClient.query(queryExplorePublications).toPromise();
+    const posts = response.data.explorePublications.items.filter((post) => {
+      if (post.profile) return post;
+      return "";
+    });
+    setPosts(posts);
+  }
+
+  async function follow(id) {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const contract = new ethers.Contract(
+      LENS_HUB_CONTRACT_ADDRESS,
+      LENSHUB,
+      provider.getSigner()
+    );
+    const tx = await contract.follow([parseInt(id)], [0x0]);
+    await tx.wait();
+  }
+
+  useEffect(() => {
+    getRecommendedProfiles();
+    getPosts();
+  }, []);
+
   return (
     <div className="app">
     </div>
